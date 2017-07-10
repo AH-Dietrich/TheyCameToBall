1b712f71df2698e03eb784301f8133f3490ff6b0
auhbz <auhbon@gmail.com>
2017-07-09 21:28:22 -0700

    game logic but tbh broken still

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+	public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		//Check for score, if 0,0 then spawn in middle, else spawn to loser
+		//give gravity stuff
+		//give initial velocity (0)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check collision with rigid body (wall, players, net)
+		//check for ground collision (score for other side)
+
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		//Debug.Log (string.Compare(collision.gameObject.ToString(),"Ground1"));
+		//check-ground 1 or 2?
+		//GameController.addScore();
+		//otherwise net or player
+		//net
+		//players
+		if(string.Compare(collision.gameObject.ToString(),"Ground1") == 1){
+			Debug.Log ("to ere");
+			gameController.addScore(1);
+		} 
+		else if(string.Compare(collision.gameObject.ToString(),"Ground2") == 1){
+			gameController.addScore(2);
+		}
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int player1Score;
+	public int player2Score;
+
+	// Use this for initialization
+	void Start () {
+		player1Score = 0;
+		player2Score = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void addScore (int whichPlayer){
+		Debug.Log ("we made it");
+		if (whichPlayer == 1) {
+			player1Score++;
+		} else if (whichPlayer == 2) {
+			player2Score++;
+		}
+	}
+}
