98fd4b11be6eb98fb11f9bf3bc24b6e5cc421519
auhbz <auhbon@gmail.com>
2017-08-01 12:41:36 -0700

    I'm committing last week's changes; we worked on the behavior/relationship between the ball and player. jumping and movement works well now. ball behavior is still off but getting better.

--- Assets/BallBehavior.cs
+++ Assets/BallBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BallBehavior : MonoBehaviour {
 	public GameController gameController;
-
+	public Rigidbody ball;
 	// Use this for initialization
 	void Start () {
 		//Check for score, if 0,0 then spawn in middle, else spawn to loser
@@ -16,10 +16,15 @@ public class BallBehavior : MonoBehaviour {
 	void Update () {
 		//check collision with rigid body (wall, players, net)
 		//check for ground collision (score for other side)
-
 	}
 	void OnCollisionEnter(Collision collision)
 	{
+		if ((collision.gameObject.tag == "Player") || (collision.gameObject.tag == "Wall")) {
+			//Debug.Log (ball.velocity);
+			Rigidbody player = collision.gameObject.GetComponent ("Rigidbody") as Rigidbody;
+			Vector3 playerVelocity = player.velocity;
+			ball.velocity = new Vector3 ( -1 * ball.velocity.x + playerVelocity.x, -1 * ball.velocity.y + playerVelocity.y, 0);
+		}
 		if(collision.gameObject.transform.name == "Ground1"){
 			Debug.Log ("to ere");
 			gameController.addScore(1);
